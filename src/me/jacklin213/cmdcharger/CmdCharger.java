3a1eaf3ae6d0c37bdb055de73caf17c88fc3ff57
jack lin <jacklin213@gmail.com>
2012-09-09 11:11:53 +1200

    stuff

--- src/me/jacklin213/cmdcharger/CmdCharger.java
+++ src/me/jacklin213/cmdcharger/CmdCharger.java
@@ -19,118 +19,140 @@ public class CmdCharger extends JavaPlugin {
 	public static CmdCharger plugin;
 	PluginDescriptionFile pdfFile;
 	private static final Logger log = Logger.getLogger("Minecraft");
-    public static Economy econ = null;
-    public static Permission perms = null;
-    public static Chat chat = null;
+	public static Economy econ = null;
+	public static Permission perms = null;
+	public static Chat chat = null;
 
-    @Override
-    public void onDisable() {
-        log.info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
-    }
+	@Override
+	public void onDisable() {
+		log.info(String.format("[%s] Disabled Version %s", getDescription()
+				.getName(), getDescription().getVersion()));
+	}
 
-    @Override
-    public void onEnable() {
-        if (!setupEconomy() ) {
-            log.warning(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
-            log.info(String.format("[%s] - Download a chat , permissions , economy and vault before using this plugin!", getDescription().getName()));
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
-        setupPermissions();
-        setupChat();
-        createconfig();
-        log.info(String.format("[%s] Enabled Version %s", getDescription().getName(), getDescription().getVersion()));
-    }
+	@Override
+	public void onEnable() {
+		if (!setupEconomy()) {
+			log.warning(String.format(
+					"[%s] - Disabled due to no Vault dependency found!",
+					getDescription().getName()));
+			log.info(String
+					.format("[%s] - Download a chat , permissions , economy and vault before using this plugin!",
+							getDescription().getName()));
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+		setupPermissions();
+		setupChat();
+		createconfig();
+		log.info(String.format("[%s] Enabled Version %s", getDescription()
+				.getName(), getDescription().getVersion()));
+	}
 
-
-    public void createconfig() {
+	public void createconfig() {
 		// Creates config.yml
-		File configfile = new File(getDataFolder() + File.separator + "config.yml");
+		File configfile = new File(getDataFolder() + File.separator
+				+ "config.yml");
 		// If config.yml doesnt exit
 		if (!configfile.exists()) {
 			// Tells console its creating a config.yml
 			this.getLogger().info("You don't have a config file!!!");
-			this.getLogger().info("Generating config.yml....."); 
+			this.getLogger().info("Generating config.yml.....");
 			this.getConfig().options().copyDefaults(true);
 			this.saveDefaultConfig();
 		}
 
-    }
-    
-    
+	}
 
 	public boolean onCommand(CommandSender sender, Command cmd,
 			String commandLabel, String[] args) {
-		if (commandLabel.equalsIgnoreCase("cmdcharge") || commandLabel.equalsIgnoreCase("cc")){
-			if (args.length >= 1){
-				if (args[0].equalsIgnoreCase("testprice")){
-					String command = args[1];
-					this.getConfig().set("Prices.Commands." + command, command);
-					this.saveConfig();
+		if (commandLabel.equalsIgnoreCase("cmdcharge")
+				|| commandLabel.equalsIgnoreCase("cc")) {
+			if (args.length >= 3) {
+					if (args[0].equalsIgnoreCase("testprice")) {
+						String command = args[1];
+						this.getConfig().set("Prices.Commands." + command,
+								command);
+						this.saveConfig();
+						try {
+							int price = Integer.parseInt(args[2]);
+							this.getConfig().set(
+									"Prices.Commands." + command + ".Price",
+									price);
+							this.saveConfig();
+							sender.sendMessage("The price for " + command
+									+ " has been set to:" + price);
+						} catch (NumberFormatException e) {
+							sender.sendMessage(ChatColor.RED
+									+ "This is not a number! Try typing a number in");
+						}
+						return true;
+					} else {
+						sender.sendMessage("testfailed");
+					}
+			}else if (args.length <= 1){
+				if (args[0].equalsIgnoreCase("help")){
 					try {
-                        int price = Integer.parseInt(args[2]);
-                        this.getConfig().set("Prices.Commands." + command + ".Price", price);
-            			this.saveConfig();
-            			sender.sendMessage("The price for " + command + " has been set to:" + price);
-                    } catch (NumberFormatException e) {
-                        sender.sendMessage(ChatColor.RED + "This is not a number! Try typing a number in");
-                    }
-					return true;
-				}else{
-					sender.sendMessage("testfailed");
-				}	
-			}else
-			if (args.length > 3){
-				if (args[0].equalsIgnoreCase("setprice") || commandLabel.equalsIgnoreCase("sp")){
-					String command = args[1];
-					try {
-                        int price = Integer.parseInt(args[2]);
-                        this.getConfig().set("Prices.Commands."+ command + ".Price" , price);
-            			this.saveConfig();
-            			sender.sendMessage("The price for " + command + "has been set to:" + price);
-                    } catch (NumberFormatException e) {
-                        sender.sendMessage(ChatColor.RED + "This is not a number! Try typing a number in");
-                    }
-					return true;
-				}else{
-					sender.sendMessage("This is not a valid command");
-					return true;
-				}
-			}else if(args.length < 3){
-				sender.sendMessage("Not enough arguments !");
-				return true;
-				}
-			
-			sender.sendMessage("test");
-			return true;
-		}
+                        int page = Integer.parseInt(args[0]);
 
+                        if (page == 1) {
+                            sender.sendMessage(ChatColor.YELLOW + " ------------ " + ChatColor.WHITE + "Help: mcRP Skills (Page 1)" + ChatColor.YELLOW + " ------------");
+                            sender.sendMessage(ChatColor.GOLD + "/cmdcharge" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Shows info");
+                            sender.sendMessage(ChatColor.GOLD + "/cmdcharge setprice [command] [price]" + ChatColor.GRAY + " - " + ChatColor.WHITE + "Set price for command!");
+                            
+                        } else {
+                            sender.sendMessage(ChatColor.RED + " Invalid page number specified. There is only 1 page right now.");
+                        }
+                    } catch (NumberFormatException nfe) {
+                        sender.sendMessage(ChatColor.RED + " Invalid page number specified. There is only 1 page right now.");
+                    }
+				}
+				
+			}
+			
+			return info(sender);
+		}
 		return false;
 	}
 
+	public boolean info(CommandSender sender){
+		sender.sendMessage(ChatColor.DARK_GREEN
+				+ "+------------------------------+");
+		sender.sendMessage(ChatColor.DARK_AQUA
+				+ "      CmdCharger: Charge people for using commands!");
+		sender.sendMessage(ChatColor.GREEN + "      By jacklin213");
+		sender.sendMessage(ChatColor.YELLOW + "      Version: "
+				+ this.pdfFile.getVersion());
+		sender.sendMessage(ChatColor.DARK_GREEN
+				+ "+------------------------------+");
+		return true;
+	}
+	
 	// Vault api import crap
 	private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-    }
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider<Economy> rsp = getServer()
+				.getServicesManager().getRegistration(Economy.class);
+		if (rsp == null) {
+			return false;
+		}
+		econ = rsp.getProvider();
+		return econ != null;
+	}
 
-    private boolean setupChat() {
-        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
-        chat = rsp.getProvider();
-        return chat != null;
-    }
+	private boolean setupChat() {
+		RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager()
+				.getRegistration(Chat.class);
+		chat = rsp.getProvider();
+		return chat != null;
+	}
 
-    private boolean setupPermissions() {
-        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
-        perms = rsp.getProvider();
-        return perms != null;
-    }
+	private boolean setupPermissions() {
+		RegisteredServiceProvider<Permission> rsp = getServer()
+				.getServicesManager().getRegistration(Permission.class);
+		perms = rsp.getProvider();
+		return perms != null;
+	}
 
 }
